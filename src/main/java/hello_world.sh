973e6f963798cc9baf13590574f0785413bfe148
javis-yan <yan_h@hotmail.com>
2018-04-15 17:41:51 +0800

    add .thrift and generate shell

---
+++ src/main/java/hello_world.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+thrift -out . --gen java hello_world.thrift
---
+++ src/main/java/hello_world.thrift
@@ -0,0 +1,6 @@
+namespace java com.nobodyhub.learn.thrift.service
+
+service HelloWorldService
+{
+    string hello(1:string name)
+}

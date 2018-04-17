4033837769a0b650a7856c7c5435b4ee86c21d44
javis-yan <yan_h@hotmail.com>
2018-04-17 08:17:24 +0800

    add calculator.thrift

---
+++ src/main/java/com/nobodyhub/learn/thrift/spring/calculator.thrift
@@ -0,0 +1,21 @@
+namespace java com.nobodyhub.learn.thrift.spring.service
+
+enum TOperation {
+    ADD = 1,
+    SUBSTRCT = 2,
+    MULTIPLY = 3,
+    DIVIDE = 4
+}
+
+exception TDivisionByZeroException {
+}
+
+service TCalculatiorService {
+    i32 calculator(
+        1: i32 num1,
+        2: i32 num2,
+        3: TOperation op
+    ) throws (
+        1: TDivisionByZeroException divisionException
+    )
+}
